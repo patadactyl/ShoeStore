ecb49eee807bd42ac7d6df89feead9f9f6be4a42
Patrick Charlton <37526417+patadactyl@users.noreply.github.com>
2021-07-23 18:14:04 -0700

    Set main activity layout to navigation controller

--- starter/app/src/main/java/com/udacity/shoestore/LoginFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/LoginFragment.kt
@@ -19,10 +19,6 @@ import com.udacity.shoestore.databinding.FragmentLoginBinding
 class LoginFragment : Fragment() {
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -37,25 +33,7 @@ class LoginFragment : Fragment() {
         binding.signupButton.setOnClickListener {
             findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
         }
-
         return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                }
-            }
-    }
 }

c336094c64971f061d2c21499c1586c0a3e66a90
Patrick Charlton <37526417+patadactyl@users.noreply.github.com>
2021-08-22 17:17:22 -0700

    Update ShoeFragment to update UI with shoeList data from ShoeViewModel using data binding.

--- starter/app/src/main/java/com/udacity/shoestore/ShoeFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/ShoeFragment.kt
@@ -6,19 +6,27 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.databinding.FragmentShoeBinding
-import com.udacity.shoestore.databinding.FragmentWelcomeBinding
+import com.udacity.shoestore.models.ShoeViewModel
 
 class ShoeFragment : Fragment() {
+
+    private lateinit var viewModel: ShoeViewModel
+    private lateinit var binding: FragmentShoeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding: FragmentShoeBinding = DataBindingUtil.inflate(
+        binding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_shoe, container, false)
+
+        viewModel = ViewModelProvider(this).get(ShoeViewModel::class.java)
+        binding.shoeViewModel = viewModel
+
         return binding.root
     }
 }
---
+++ starter/app/src/main/java/com/udacity/shoestore/models/ShoeViewModel.kt
@@ -0,0 +1,48 @@
+package com.udacity.shoestore.models
+
+import android.text.format.DateUtils
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+
+class ShoeViewModel: ViewModel() {
+
+    private val _shoe = MutableLiveData<String>()
+    val shoe : LiveData<String>
+        get() = _shoe
+
+    private lateinit var shoeList: MutableList<String>
+
+    init {
+        resetList()
+    }
+
+    private fun resetList() {
+        shoeList = mutableListOf(
+            "queen",
+            "hospital",
+            "basketball",
+            "cat",
+            "change",
+            "snail",
+            "soup",
+            "calendar",
+            "sad",
+            "desk",
+            "guitar",
+            "home",
+            "railway",
+            "zebra",
+            "jelly",
+            "car",
+            "crow",
+            "trade",
+            "bag",
+            "roll",
+            "bubble"
+        )
+        shoeList.shuffle()
+    }
+}
+

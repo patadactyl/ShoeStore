ea6a84b2571502480b99ae7e454c52ee96752570
Patrick Charlton <37526417+patadactyl@users.noreply.github.com>
2021-10-10 12:43:39 -0700

    Updated Detail Fragment

--- starter/app/src/main/java/com/udacity/shoestore/DetailFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/DetailFragment.kt
@@ -7,21 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.databinding.FragmentDetailBinding
 import com.udacity.shoestore.databinding.FragmentShoeBinding
 import com.udacity.shoestore.models.ShoeDetailViewModel
 import com.udacity.shoestore.models.ShoeViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DetailFragment : Fragment() {
 
     private lateinit var viewModel: ShoeDetailViewModel
@@ -34,6 +26,9 @@ class DetailFragment : Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_detail, container, false)
+        binding.enterDetailsButton.setOnClickListener {
+            findNavController().navigate(DetailFragmentDirections.actionDetailFragmentToShoeFragment())
+        }
 
         viewModel = ViewModelProvider(this).get(ShoeDetailViewModel::class.java)
         binding.shoeDetailViewModel = viewModel
